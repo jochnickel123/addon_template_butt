35d4135fa382e75f34aedd49318f6043991958e0
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-01-19 23:53:04 +0100

    Update startitems.lua

--- game/dota_addons/addon_template_butt/scripts/vscripts/startitems.lua
+++ game/dota_addons/addon_template_butt/scripts/vscripts/startitems.lua
@@ -7,6 +7,7 @@ local startitems = {
 }
 local bonusabilities = {
 	-- exampleability = { lvl = 1, cd = 120 , nokey = true, hidden = true, cast = true },
+	exampleability = { lvl = 1 }
 	-- roshan_spell_block = { lvl = 4, nokey = true },
 }
 local bonusmodifier = {
